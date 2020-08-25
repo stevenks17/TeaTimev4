73b7d250c6516339690d6db498fd5473c4db7bef
stevenks17 <stevenks17@gmail.com>
2020-08-25 15:54:16 -0400

    generated reviews model

---
+++ db/migrate/20200825195343_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reviews do |t|
+      t.integer :rating
+      t.string :title
+      t.text :content
+      t.belongs_to :tea, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
