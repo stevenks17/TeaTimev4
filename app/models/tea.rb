ece08d4e1be491d1dcfdf29b9715a7cff91691dd
stevenks17 <stevenks17@gmail.com>
2020-08-26 12:57:48 -0400

    added relationshp nested attr and validations

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,3 +1,9 @@
 class Tea < ApplicationRecord
   belongs_to :brand
+  has_many :reviews
+  accepts_nested_attributes_for :brand
+
+  validates :flavor, presence:true  
+  validate :not_a_duplicate
+  validates :description, presence:true
 end
