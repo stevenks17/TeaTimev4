5788865510c23ae2678bd7047fc407f389e7ce71
stevenks17 <stevenks17@gmail.com>
2020-08-26 13:19:32 -0400

    added validation and scope from teatimeV3

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,5 @@
 class Brand < ApplicationRecord
     has_many :teas
+    validates :name, presence: true, uniqueness: true
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :tea
+
+  validates :content, presence: true
+  validates :title, presence: true
+  validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6}
+  
+  scope :order_by_rating, ->{group(:id).order('avg(reviews.rating) desc')}
+
 end
