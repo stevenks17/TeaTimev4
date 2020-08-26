41c4dc9cde836439d5412909f1da44a062b5571a
stevenks17 <stevenks17@gmail.com>
2020-08-26 13:22:34 -0400

    adjusted route allowance for tea to reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :reviews
-  resources :teas
+
+  resources :teas do
+    resources :reviews, only: [:new, :index]
+  end
+
   resources :brands
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
